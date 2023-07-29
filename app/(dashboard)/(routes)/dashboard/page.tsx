5f23b11cfe2cefa8824ae1af59b3b0c95eb3cbf8
Sunil Kumar P <sunilkpg23@gmail.com>
2023-07-29 10:11:37 +0530

    little changes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -4,7 +4,7 @@ export default function DashboardPage() {
   return (
     <div>
       <Button variant="destructive">Dashboard Page</Button>
-      {/* <UserButton/> */}
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
